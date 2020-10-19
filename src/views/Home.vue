5dbc8d0aa2808b592da0785bcd526a11ca3d2737
tielenn <mcore.1212@gmail.com>
2020-10-19 22:30:48 +0300

    final

---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h2>Home page</h2>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci alias ipsum iure laboriosam minus molestiae pariatur reiciendis rem, sequi sunt.</p>
+
+    <router-link to="/todos">Todos</router-link>
+  </div>
+</template>
